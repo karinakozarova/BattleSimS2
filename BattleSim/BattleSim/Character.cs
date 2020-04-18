1ed9256b54fd3bfbfe36c677f05b33c243b1ba78
Karina Kozarova <karinakozarova1303@gmail.com>
2020-04-18 15:01:28 +0200

    Implement as much as possible of the Character class

--- BattleSim/BattleSim/Character.cs
+++ BattleSim/BattleSim/Character.cs
@@ -8,5 +8,37 @@ namespace BattleSim
 {
     class Character
     {
+        public string Name
+        {
+            get;
+            private set;
+        }   
+        
+        public double Health
+        {
+            get;
+            private set;
+        }
+
+        public Weapon EquipedWeapon
+        {
+            get;
+            private set;
+        }
+
+        public double ReceiveDamage()
+        {
+            throw new NotImplementedException();
+        } 
+        
+        public double DealDamage()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} with {EquipedWeapon} has {Health} health";
+        }
     }
 }
