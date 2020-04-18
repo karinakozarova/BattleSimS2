02f4714f3c24e103715742e09f55d4d9704409f4
Karina Kozarova <karinakozarova1303@gmail.com>
2020-04-18 16:50:07 +0200

    start weapon class

--- BattleSim/BattleSim/Weapon.cs
+++ BattleSim/BattleSim/Weapon.cs
@@ -8,5 +8,21 @@ namespace BattleSim
 {
     class Weapon: Attack
     {
+        public Weapon(string name, double damage): base(name, damage) { }
+
+        public static List<Weapon> GetAllWeaponsFromDatabase()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void SaveObjectToDb()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ToString()
+        {
+            return $"This weapon ({Name}) has damage {Damage}";
+        }
     }
 }
