114359ba635c89a6fd5f791d828185cdecfcc02e
Karina Kozarova <karinakozarova1303@gmail.com>
2020-04-18 17:11:33 +0200

    add spell class

--- BattleSim/BattleSim/Spell.cs
+++ BattleSim/BattleSim/Spell.cs
@@ -9,5 +9,29 @@ namespace BattleSim
 {
     class Spell: Attack
     {
+        public double ManaCost
+        {
+            get;
+            private set;
+        }
+
+        public Spell(string name, double damage, double manaCost) : base(name, damage) {
+            this.ManaCost = manaCost;
+        }
+
+        public static List<Spell> GetAllSpellsFromDatabase()
+        {
+            throw new NotImplementedException();
+        }
+
+        public static void SaveObjectToDb()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ToString()
+        {
+            return $"This spell ({Name}) has damage {Damage} and costs {ManaCost} mana";
+        }
     }
 }
