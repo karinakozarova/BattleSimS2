fd29b39015ac39a374d6ad939bd75009cc9f9e28
Evgeniy Terziev <i425986@fhict.nl>
2020-04-21 14:22:52 +0300

    Added unsaved files

--- BattleSim/BattleSim/WizardControl.cs
+++ BattleSim/BattleSim/WizardControl.cs
@@ -8,14 +8,10 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-using System.Runtime.InteropServices;
-
 namespace BattleSim
 {
     public partial class WizardControl : UserControl
     {
-        sbyte progessBarRed = 2;
-        sbyte progessBarYellow = 3;
         Player player;
         public WizardControl(Player player)
         {
@@ -25,7 +21,6 @@ namespace BattleSim
             lblPlayerName.Text = player.PlayerName + " with " + wizard.Name;
             pbrPlayerHealth.Value = (int)player.SelectedCharacter.Health;
             pbrPlayerMana.Value = (int)wizard.Mana;
-            pbrPlayerMana.ForeColor = Color.Blue;
         }
 
 
@@ -38,20 +33,5 @@ namespace BattleSim
             pbrPlayerMana.Value = mana;
         }
 
-        private void WizardControl_Load(object sender, EventArgs e)
-        {
-            ModifyProgressBarColor.SetState(pbrPlayerMana, progessBarRed);
-            ModifyProgressBarColor.SetState(pbrPlayerHealth, progessBarYellow);
-        }
-        
-    }
-    public static class ModifyProgressBarColor
-    {
-        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = false)]
-        static extern IntPtr SendMessage(IntPtr hWnd, uint Msg, IntPtr w, IntPtr l);
-        public static void SetState(this ProgressBar pBar, int state)
-        {
-            SendMessage(pBar.Handle, 1040, (IntPtr)state, IntPtr.Zero);
-        }
     }
 }
